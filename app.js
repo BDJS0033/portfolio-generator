39ea34a533f33d958fd0172f165cebbe3b0c2988
SAB <sevanboyadjian@gmail.com>
2022-02-16 22:15:34 -0500

    app.js

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-
-const generatePage = (userName, githubName) => {
-  return `
-    Name: ${userName}
-    GitHub: ${githubName}
-  `;
-};
-=======
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
@@ -22,4 +11,3 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
   console.log('Portfolio complete! Check out index.html to see the output!');
 
 });
->>>>>>> aa6314f4b8c964612a29394c23840e2d8d08c5cc
